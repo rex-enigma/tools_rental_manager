be78d9f9b50de26fe2903ce53bc4ac7d6ed0a1ed
josh <infinityjosh.jn@gmail.com>
2023-08-15 20:25:42 +0300

    vertically centered the contents of the CustomListTiles in ToolUsersView

--- lib/ui/views/tool_users/tool_users_view.dart
+++ lib/ui/views/tool_users/tool_users_view.dart
@@ -46,11 +46,9 @@ class ToolUsersView extends StackedView<ToolUsersViewModel> {
               onPressed: viewModel.toolUsers.isEmpty
                   ? null
                   : () {
-                      viewModel.showAppBarSearchField =
-                          !viewModel.showAppBarSearchField;
+                      viewModel.showAppBarSearchField = !viewModel.showAppBarSearchField;
                       // if viewModel.showAppBarSearchField = false, (the user cancelled search) we reset filtered tool users to default
-                      if (!viewModel.showAppBarSearchField)
-                        viewModel.resetFilteredToolUsersToDefault();
+                      if (!viewModel.showAppBarSearchField) viewModel.resetFilteredToolUsersToDefault();
                     },
               icon: viewModel.showAppBarSearchField
                   ? Icon(
@@ -60,9 +58,7 @@ class ToolUsersView extends StackedView<ToolUsersViewModel> {
                   : Icon(
                       Icons.search,
                       // use grey color to indicate that the button is disabled when viewMode.toolUsers is empty
-                      color: viewModel.toolUsers.isEmpty
-                          ? Theme.of(context).disabledColor
-                          : Theme.of(context).colorScheme.secondary,
+                      color: viewModel.toolUsers.isEmpty ? Theme.of(context).disabledColor : Theme.of(context).colorScheme.secondary,
                     ),
             ),
           ],
@@ -85,14 +81,12 @@ class ToolUsersView extends StackedView<ToolUsersViewModel> {
                   (toolUser) {
                     return InkWell(
                       onTap: () {
-                        print(
-                            '${toolUser.firstName} has been  tapped'); // its here for testing
+                        print('${toolUser.firstName} has been  tapped'); // its here for testing
                       },
                       child: Padding(
-                        padding: const EdgeInsets.only(
-                            left: 16.0, right: 5.0, top: 10.0, bottom: 10.0),
+                        padding: const EdgeInsets.only(left: 16.0, right: 5.0, top: 10.0, bottom: 10.0),
                         child: CustomListTile(
-                          contentVerticalAlignment: CrossAxisAlignment.start,
+                          contentVerticalAlignment: CrossAxisAlignment.center,
                           leading: Container(
                             width: 90,
                             height: 90,
@@ -116,12 +110,9 @@ class ToolUsersView extends StackedView<ToolUsersViewModel> {
                           ),
                           title: Text(
                             '${toolUser.firstName} ${toolUser.lastName}',
-                            style: switch (
-                                getThemeManager(context).selectedThemeMode) {
-                              ThemeMode.light =>
-                                Theme.of(context).typography.white.titleMedium!,
-                              ThemeMode.dark =>
-                                Theme.of(context).typography.black.titleMedium!,
+                            style: switch (getThemeManager(context).selectedThemeMode) {
+                              ThemeMode.light => Theme.of(context).typography.white.titleMedium!,
+                              ThemeMode.dark => Theme.of(context).typography.black.titleMedium!,
                               _ => throw ' configure ThemeMode.system',
                             },
                           ),
@@ -134,13 +125,11 @@ class ToolUsersView extends StackedView<ToolUsersViewModel> {
                                   children: [
                                     TextSpan(
                                       text: 'Phone number : ',
-                                      style: subtitleFirstSubStringTextStyle(
-                                          context),
+                                      style: subtitleFirstSubStringTextStyle(context),
                                     ),
                                     TextSpan(
                                       text: toolUser.phoneNumber.toString(),
-                                      style: subtitleLastSubStringTextStyle(
-                                          context),
+                                      style: subtitleLastSubStringTextStyle(context),
                                     ),
                                   ],
                                 ),
@@ -151,32 +140,28 @@ class ToolUsersView extends StackedView<ToolUsersViewModel> {
                                   children: [
                                     TextSpan(
                                       text: 'Tool count : ',
-                                      style: subtitleFirstSubStringTextStyle(
-                                          context),
+                                      style: subtitleFirstSubStringTextStyle(context),
                                     ),
                                     TextSpan(
-                                      text: toolUser.tools?.length.toString() ??
-                                          '0',
-                                      style: subtitleLastSubStringTextStyle(
-                                          context),
+                                      text: toolUser.tools?.length.toString() ?? '0',
+                                      style: subtitleLastSubStringTextStyle(context),
                                     ),
                                   ],
                                 ),
                               ),
                             ],
                           ),
-                          trailing: toolUser.tools ==
-                                  null // if toolUser.tools is null, it means that the tool user is not using any tool (no tool(s) is associated with  that tool user)
-                              ? IconButton(
-                                  visualDensity: VisualDensity.compact,
-                                  iconSize: 26,
-                                  icon: const Icon(Icons.delete),
-                                  onPressed: () {
-                                    print(
-                                        '${toolUser.firstName} will be deleted'); // implement a delete functionality to delete retired tools
-                                  },
-                                )
-                              : null,
+                          trailing:
+                              toolUser.tools == null // if toolUser.tools is null, it means that the tool user is not using any tool (no tool(s) is associated with  that tool user)
+                                  ? IconButton(
+                                      visualDensity: VisualDensity.compact,
+                                      iconSize: 26,
+                                      icon: const Icon(Icons.delete),
+                                      onPressed: () {
+                                        print('${toolUser.firstName} will be deleted'); // implement a delete functionality to delete retired tools
+                                      },
+                                    )
+                                  : null,
                         ),
                       ),
                     );
