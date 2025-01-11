11c83767fe14d73cb03984ad5cf30aeccc996dda
josh <infinityjosh.jn@gmail.com>
2025-01-11 10:00:13 +0300

    made the methods that accepted or returned a Model | list of Models to accept or return Entity | list of Entities

--- lib/ui/bottom_sheets/select_tool/select_tool_sheet_model.dart
+++ lib/ui/bottom_sheets/select_tool/select_tool_sheet_model.dart
@@ -1,28 +1,27 @@
 import 'package:stacked/stacked.dart';
 import 'package:tools_rental_management/app/app.locator.dart';
-import 'package:tools_rental_management/data/models/tool_model.dart';
 import 'package:tools_rental_management/data/repositories/tools/tools_repo_imp.dart';
+import 'package:tools_rental_management/domain/entities/tool_entity.dart';
 import 'package:tools_rental_management/enums/status.dart';
-import 'package:tools_rental_management/ui/views/tools/tools_viewmodel.dart';
 
 class SelectToolSheetModel extends BaseViewModel {
   final _toolsRepoImp = locator<ToolsRepoImp>();
   // these are tools whose status is idle
-  List<ToolModel> idleTools = [];
-  List<ToolModel> selectedIdleTools = [];
+  List<ToolEntity> idleTools = [];
+  List<ToolEntity> selectedIdleTools = [];
   bool isAnyToolSelected = false;
 
   void initState() async {
     await fetchIdleTools();
   }
 
-  void selectTool(ToolModel tool) {
+  void selectTool(ToolEntity tool) {
     isAnyToolSelected = true;
     selectedIdleTools.add(tool);
     rebuildUi();
   }
 
-  void deselectIdleTool(ToolModel toolIdle) {
+  void deselectIdleTool(ToolEntity toolIdle) {
     selectedIdleTools.remove(toolIdle);
     if (selectedIdleTools.isEmpty) isAnyToolSelected = false;
     rebuildUi();
@@ -35,7 +34,7 @@ class SelectToolSheetModel extends BaseViewModel {
   }
 
   Future<void> fetchIdleTools() async {
-    List<ToolModel>? idleTools = await runBusyFuture(_toolsRepoImp.getToolsByStatusOrNull(Status.idle));
+    List<ToolEntity>? idleTools = await runBusyFuture(_toolsRepoImp.getToolsByStatusOrNull(Status.idle));
     // only add to the this.idleTools if the idleTools is not null
     if (idleTools != null) {
       // order the idleTools in descending order to display the newly add idle tool in the database at the top in th UI
