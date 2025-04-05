951fd320f32ddae2a451a2231e0b30808c7cf15c
josh <infinityjosh.jn@gmail.com>
2025-04-05 14:14:12 +0300

    .

--- test/viewmodels/home_viewmodel_test.dart
+++ test/viewmodels/home_viewmodel_test.dart
@@ -7,34 +7,34 @@ import 'package:tools_rental_management/ui/views/home/home_viewmodel.dart';
 
 import '../helpers/test_helpers.dart';
 
-// void main() {
-//   HomeViewModel _getModel() => HomeViewModel();
+void main() {
+  // HomeViewModel _getModel() => HomeViewModel();
 
-//   group('HomeViewmodelTest -', () {
-//     setUp(() => registerServices());
-//     tearDown(() => locator.reset());
+  // group('HomeViewmodelTest -', () {
+  //   setUp(() => registerServices());
+  //   tearDown(() => locator.reset());
 
-//     group('incrementCounter -', () {
-//       test('When called once should return  Counter is: 1', () {
-//         final model = _getModel();
-//         model.incrementCounter();
-//         expect(model.counterLabel, 'Counter is: 1');
-//       });
-//     });
+  //   group('incrementCounter -', () {
+  //     test('When called once should return  Counter is: 1', () {
+  //       final model = _getModel();
+  //       model.incrementCounter();
+  //       expect(model.counterLabel, 'Counter is: 1');
+  //     });
+  //   });
 
-//     group('showBottomSheet -', () {
-//       test('When called, should show custom bottom sheet using notice variant',
-//           () {
-//         final bottomSheetService = getAndRegisterBottomSheetService();
+  //   group('showBottomSheet -', () {
+  //     test('When called, should show custom bottom sheet using notice variant',
+  //         () {
+  //       final bottomSheetService = getAndRegisterBottomSheetService();
 
-//         final model = _getModel();
-//         model.showBottomSheet();
-//         verify(bottomSheetService.showCustomSheet(
-//           variant: BottomSheetType.notice,
-//           title: ksHomeBottomSheetTitle,
-//           description: ksHomeBottomSheetDescription,
-//         ));
-//       });
-//     });
-//   });
-// }
+  //       final model = _getModel();
+  //       model.showBottomSheet();
+  //       verify(bottomSheetService.showCustomSheet(
+  //         variant: BottomSheetType.notice,
+  //         title: ksHomeBottomSheetTitle,
+  //         description: ksHomeBottomSheetDescription,
+  //       ));
+  //     });
+  //   });
+  // });
+}
